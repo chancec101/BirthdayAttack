8b9169a2832ce1b36cc0525cab3522e41494ed47
Chance Currie <71950502+chancec101@users.noreply.github.com>
2023-04-06 15:00:59 -0500

    Added more comments and added dictionary for sorting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import hashlib
 import random
+import operator
 
+'''Function to get the first 44 bits of SHA-256'''
 def BadHash44(msg):
         
 	msg = msg.encode()
@@ -8,23 +10,28 @@ def BadHash44(msg):
 
 	return hashMsg[0:11]
 
+'''Function to find a hash collision'''
 def find_collision():
 
-    n = 256
-    file = open("hash.csv", "w")
+    n = 256     # stores length of input message
+    file = open("hash.csv", "w")    # file to store hash data
 
-    x0 = random.getrandbits(n)
-    x0 = hex(x0)[2:]
+    x0 = random.getrandbits(n)  # generate random 256 bit value
+    x0 = hex(x0)[2:]    # convert to hex
 
+    # loop to make sure length of hex message is 265 bits
     while len(x0) != 64:
         x0 = "0" + x0
                 
     x = x0
     x_prime = x
+
+    hash_dict = {}  #dictionary to store hash data
     
     i = 0
     while i < 2**(n/2):
-        
+
+        #create hash chain and check if they match
         x = BadHash44(x)
         x_prime = BadHash44(BadHash44(x_prime))
         if x == x_prime:
@@ -37,18 +44,29 @@ def find_collision():
     
     for j in range(i):
 
-        file.write(f"{x}, {BadHash44(x)}\n")
-        file.write(f"{x_prime}, {BadHash44(x_prime)}\n")
+        hash_dict[x] = BadHash44(x)
+        hash_dict[x_prime] = BadHash44(x_prime)
 
+        # if matching hash, collision found
         if BadHash44(x) == BadHash44(x_prime):
             
             print(f"x hash: {BadHash44(x)}")
             print(f"x prime hash: {BadHash44(x_prime)}")
 
+            # sort hash data
+            new_dict = sorted(hash_dict.items(), key=operator.itemgetter(1))
+            new_dict = dict(new_dict)
+
+            #write hashes to file 
+            for key, value in new_dict.items():
+                file.write(f"{key} -> {value}\n")
+
+
             file.close()
 
             return x, x_prime
         
+        # update hash
         x = BadHash44(x)
         x_prime = BadHash44(x_prime)
     
